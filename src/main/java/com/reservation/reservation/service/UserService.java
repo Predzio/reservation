48110908a26f88521f819de440a2bac9f5b1a1bb
Adrian <adrian2679@poczta.onet.pl>
2025-11-13 12:20:25 +0100

    Added User service

---
+++ src/main/java/com/reservation/reservation/service/UserService.java
@@ -0,0 +1,52 @@
+package com.reservation.reservation.service;
+
+import com.reservation.reservation.dto.request.CreateDoctorRequest;
+import com.reservation.reservation.dto.request.RegisterRequest;
+import com.reservation.reservation.model.Role;
+import com.reservation.reservation.model.User;
+import com.reservation.reservation.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User registerNewPatient(RegisterRequest request) {
+        if(userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Error: Email is already in use!");
+        }
+
+        User user = new User(
+                request.getEmail(),
+                passwordEncoder.encode(request.getPassword()),
+                request.getFirstName(),
+                request.getLastName(),
+                Set.of(Role.ROLE_PATIENT)
+        );
+
+        return userRepository.save(user);
+    }
+
+    public User createNewDoctor(CreateDoctorRequest request) {
+        if(userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Error: Email is already in use!");
+        }
+
+        User user = new User(
+                request.getEmail(),
+                passwordEncoder.encode(request.getPassword()),
+                request.getFirstName(),
+                request.getLastName(),
+                Set.of(Role.ROLE_DOCTOR)
+        );
+
+        return userRepository.save(user);
+    }
+}
