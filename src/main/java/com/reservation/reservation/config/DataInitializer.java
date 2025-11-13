a4b3345f78910add62b7aaeae950fe660231fd25
Adrian <adrian2679@poczta.onet.pl>
2025-11-13 13:04:50 +0100

    Added data initializer to register Admin to database

---
+++ src/main/java/com/reservation/reservation/config/DataInitializer.java
@@ -0,0 +1,41 @@
+package com.reservation.reservation.config;
+
+import com.reservation.reservation.model.Role;
+import com.reservation.reservation.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import com.reservation.reservation.model.User;
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j //logs in console
+public class DataInitializer implements CommandLineRunner {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        final String adminEmail = "admin@company.com";
+        if(!userRepository.existsByEmail(adminEmail)) {
+            log.info("Creating initial ADMIN user...");
+            final String adminPassword = "dxxxxx0xxxxxxxxnxxxxzDxxxxxfxxxxxxd";
+            User admin = new User(
+                    adminEmail,
+                    passwordEncoder.encode(adminPassword),
+                    "Super",
+                    "Admin",
+                    Set.of(Role.ROLE_DOCTOR, Role.ROLE_PATIENT, Role.ROLE_ADMIN)
+            );
+            userRepository.save(admin);
+
+            log.info("ADMIN user created successfully.");
+        } else {
+            log.info("ADMIN user already exists.");
+        }
+    }
+}
+
