669149038b57d773e0ec78b2339cde62f41d4b1c
Adrian <adrian2679@poczta.onet.pl>
2025-12-16 09:03:18 +0100

    Added @EnableAsync adnotation to main class

--- src/main/java/com/reservation/reservation/ReservationApplication.java
+++ src/main/java/com/reservation/reservation/ReservationApplication.java
@@ -3,10 +3,12 @@ package com.reservation.reservation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
 @Slf4j
+@EnableAsync
 public class ReservationApplication {
 
 	public static void main(String[] args) {
